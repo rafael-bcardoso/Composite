afaa1fc6342e7eb6c490a178c7c9155624b0ade8
Rafael Cardoso <rafael_eng31@outlook.com>
2020-02-23 16:16:11 -0300

    Implementando a classe cliente. Uma coisa que devemos destacar é que o método CountGift e TotalPrice são recursivos e passam em cada um dos items da listas. Se um item dessa lista também for uma lista, primeiro ele resolve o valor total que tem dentro dessa lista e depois soma com o que já estava sendo calculado inicialmente.

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -6,7 +6,30 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var root = new CompositeGift("Box", 0);
+
+            root.AddGift(new Gift("Phone", 100));
+            root.AddGift(new Gift("Toy", 10));
+            root.AddGift(new Gift("Bicycle", 200));
+
+
+            var childBox = new CompositeGift("ChildBox", 0);
+            childBox.AddGift(new Gift("Game", 3000));
+            childBox.AddGift(new Gift("mini car", 9000));
+
+            var specialBox = new CompositeGift("MiniBox", 0);
+            specialBox.AddGift(new Gift("Computer", 1000));
+            specialBox.AddGift(new Gift("TV", 1000));
+            specialBox.AddGift(new Gift("Play Station", 10000));
+
+            childBox.AddGift(specialBox);
+            root.AddGift(childBox);
+
+            root.AddGift(new Gift("Ball", 909090));
+
+            Console.WriteLine($"Nessa caixa de brinquedos há um total de: {root.CountGift()}");
+            Console.WriteLine($"O valor total gasto nesses presentes foi: {root.TotalPrice()}");
         }
     }
 }
