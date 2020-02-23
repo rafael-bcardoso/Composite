ebffe6bb8a94d3b7358658030e1ae4c74ca84f0d
Rafael Cardoso <rafael_eng31@outlook.com>
2020-02-23 15:45:54 -0300

    Create project and create class Gift base with default operations

---
+++ Composite/GiftBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Composite
+{
+    public abstract class GiftBase
+    {
+        protected GiftBase(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        protected string Name { get; private set; }
+        protected decimal Price { get; private set; }
+
+        public abstract decimal TotalPrice();
+        public abstract void RemoveGift();
+        public abstract void AddGift();
+        public abstract int CountGift();
+    }
+}
